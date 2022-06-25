110eff89f99416a72b25d38e7a2bc893853fa4cd
Damilola Mohammed <damilolamohammed2@hotmail.com>
2022-06-25 11:41:57 +0100

    console.log removed

--- main.js
+++ main.js
@@ -5,7 +5,6 @@ const hands = document.querySelectorAll('.hand')
 function setDate() {
     hands.forEach( h => {
         if(h.style.transform === `rotate(438deg)` || h.style.transform === `rotate(444deg)` || h.style.transform === `rotate(90deg)` || h.style.transform === `rotate(450deg)` ) {
-            console.log(h.style.transform)
             h.style.transition = '0s'
         }else {
             h.style.transition = '0.05s'
@@ -14,12 +13,10 @@ function setDate() {
     const currentTime = new Date()
     const seconds = currentTime.getSeconds()
     const secondsDegrees = ((seconds / 60) * 360) + 90
-    let s = secondsHand.style.transform = `rotate(${secondsDegrees}deg)`
+    secondsHand.style.transform = `rotate(${secondsDegrees}deg)`
     const minutes = currentTime.getMinutes()
-    console.log(minutes)
     const minutesDegrees = ((minutes / 60) * 360) + 90
     minutesHand.style.transform = `rotate(${minutesDegrees}deg)`
-    // console.log(s)
     const hour = currentTime.getHours()
     const hourDegrees = ((hour / 60) * 360) + 90
     hourHand.style.transform = `rotate(${hourDegrees}deg)`
